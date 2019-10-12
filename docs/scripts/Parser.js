39d21b5a38d2f1a771b0bb08c885518fdc22fb84
tko <tko@jitu.org>
2019-10-12 18:00:28 +0900

    refactor for next stage.

--- docs/scripts/Parser.js
+++ docs/scripts/Parser.js
@@ -4,17 +4,23 @@ export default class {
 	parse(text) {
 		var res = "";
 		var lines = this.breakLines(text);
-		for (var i = 0; i < lines.length; ++i) {
-			res += this.parseLine(lines[i]) + "\n";
-		}
-		return res;
+		var recs = this.parseLines(lines);
+		return this.writeRecords(recs);
 	}
 
 	breakLines(text) {
 		return text.split(/\r\n|\r|\n/);
 	}
 
-	parseLine(line) {
+	parseLines(lines) {
+		var recs = [];
+		for (var i = 0; i < lines.length; ++i) {
+			recs.push(this.parseRecord(lines[i]));
+		}
+		return recs;
+	}
+
+	parseRecord(line) {
 		var ln = line.trim();
 		var ary = ln.split(/\s+/, 3);
 		if (ary.length < 2) {
@@ -27,5 +33,13 @@ export default class {
 		var hit = parseInt(ary[1]);
 		var comment = (ary.length == 3) ? ary[2] : "";
 		return new Record(total, hit, comment).toString();
+	}
+
+	writeRecords(recs) {
+		var txt = "";
+		for (var i = 0; i < recs.length; ++i) {
+			txt += recs[i].toString() + "\n";
+		}
+		return txt;
 	}
 }
