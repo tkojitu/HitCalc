946b26dbd6fa444c5b80890cd1d7e5054f5a791c
tko <tko@jitu.org>
2019-10-12 16:01:35 +0900

    modify html layout and add clrar button.

--- docs/scripts/Calc.js
+++ docs/scripts/Calc.js
@@ -1,57 +1,78 @@
 export default class {
-    setup() {
-        var self = this;
-        var b = document.getElementById("buttonCalc");
-        b.addEventListener(
-            "click",
-            function() {
-                self.calc();
-            });
-    }
+	setup() {
+		this.setupButtonCalc();
+		this.setupButtonClear();
+	}
+	
+	setupButtonCalc() {
+		var self = this;
+		var b = document.getElementById("buttonCalc");
+		b.addEventListener(
+			"click",
+			function() {
+				self.calc();
+			});
+	}
 
-    calc() {
-        var t = document.getElementById("book");
-        var res = this.calcLines(t.value);
-        t.value = res;
-    }
+	setupButtonClear() {
+		var self = this;
+		var b = document.getElementById("buttonClear");
+		b.addEventListener(
+			"click",
+			function() {
+				self.clear();
+			});
+	}
 
-    calcLines(text) {
-        var res = "";
-        var lines = this.breakLines(text);
-        for (var i = 0; i < lines.length; ++i) {
-            res += this.parseLine(lines[i]) + "\n";
-        }
-        return res;
-    }
+	calc() {
+		var t = document.getElementById("book");
+		var res = this.calcLines(t.value);
+		t.value = res;
+	}
 
-    breakLines(text) {
-        return text.split(/\r\n|\r|\n/);
-    }
+	calcLines(text) {
+		var res = "";
+		var lines = this.breakLines(text);
+		for (var i = 0; i < lines.length; ++i) {
+			res += this.parseLine(lines[i]) + "\n";
+		}
+		return res;
+	}
 
-    parseLine(line) {
-        var ln = line.trim();
-        var ary = ln.split(/\s+/, 3);
-        if (ary.length < 2) {
-            return line;
-        }
-        var total = parseInt(ary[0], 10);
-        if (total == 0) {
-            return line;
-        }
-        var hit = parseInt(ary[1]);
-        var comment = (ary.length == 3) ? ary[2] : "";
-        var rate = this.calcRate(total, hit);
-        return this.rateToString(total, hit, comment, rate);
-    }
+	breakLines(text) {
+		return text.split(/\r\n|\r|\n/);
+	}
 
-    calcRate(total, hit) {
-        if (total == 0) {
-            return 0;
-        }
-        return hit / total * 100.0;
-    }
+	parseLine(line) {
+		var ln = line.trim();
+		var ary = ln.split(/\s+/, 3);
+		if (ary.length < 2) {
+			return line;
+		}
+		var total = parseInt(ary[0], 10);
+		if (total == 0) {
+			return line;
+		}
+		var hit = parseInt(ary[1]);
+		var comment = (ary.length == 3) ? ary[2] : "";
+		var rate = this.calcRate(total, hit);
+		return this.rateToString(total, hit, comment, rate);
+	}
 
-    rateToString(total, hit, comment, rate) {
-        return "" + total + " " + hit + " " + comment + " " + rate.toFixed(2);
-    }
+	calcRate(total, hit) {
+		if (total == 0) {
+			return 0;
+		}
+		return hit / total * 100.0;
+	}
+
+	rateToString(total, hit, comment, rate) {
+		return "" + total + " " + hit + " " + comment + " " + rate.toFixed(2);
+	}
+
+	clear() {
+		var t = document.getElementById("book");
+		t.value = "";
+	}
 }
+
